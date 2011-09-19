952705e4db500c4d2a4d2609475def01d607fb04
Marty Nelson <marty.nelson@yahoo.com>
2011-09-19 12:20:29 -0700

    use full path in fs call

--- server.js
+++ server.js
@@ -4,25 +4,24 @@ var express = require('express')
  
 var app = express.createServer(
     express.static(path.join(__dirname, 'public'))
-);
+)
 
-app.set('view engine', 'jade');
+app.set('view engine', 'jade')
 
 
 app.get('/', function (request, response) {
     //response.render('meepbop', { layout: false });
-});
+})
 
-
-
+var templatePath = path.join(__dirname, 'features/templates')
 app.get('/templates/:name', function (request, response) {
-    fs.readFile('features/templates/' + request.params.name + '.jade', function(err, data) {
+    fs.readFile(path.join(templatePath, request.params.name + '.jade'), function(err, data) {
         response.send(err ? { err: err } : { template: data.toString() });
     })
     
-});
+})
 
 
 var port = 11848 // 80;
-app.listen(port);
-console.log('Jade-editor started on port ' + port);
+app.listen(port)
+console.log('Jade-editor started on port ' + port)
