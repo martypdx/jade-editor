5481c6c510d1103ae28b1268dd27f59f903649d8
Marty Nelson <marty.nelson@yahoo.com>
2012-11-01 10:35:08 -0700

    trying to update with something that works

--- sample-app/features/demo/bowling2/bowling2.js
+++
@@ -1,9 +0,0 @@
-var game = $('.game')
-var adjustSize = function() {
-    var width = game.width()/10
-    game.css('height', game.width()/10)
-	game.css('font-size',  width/3.5 + 'px')
-}
-adjustSize()
-$(window).resize(adjustSize)
-
